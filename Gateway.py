aba9d963ea6b43e90f1c1dac49fc4f5d17ad1211
Martin <martinvdberg@protonmail.com>
2021-05-21 17:23:09 +0200

    serialization and adding device type via web service

--- Gateway.py
+++ Gateway.py
@@ -4,11 +4,12 @@ import signal
 from datetime import datetime
 import re
 import RFLinkTools
+import pickle
 
 
 class Gateway:
     def __signal_handler(self, frame):  # TODO - not sure if this is correct
-        logging.info("Gateway exiting gracefully")
+        logging.info('Gateway exiting gracefully')
         self.running = False
 
     def __init__(self):
@@ -16,10 +17,13 @@ class Gateway:
         # set-up the basic device administration
         self.device_types = []
         self.running = True
+        self.filename = './gateway.db'
         signal.signal(signal.SIGINT, self.__signal_handler)
 
     def add_device_type(self, device_type):
-        self.device_types.append(device_type)
+        i = max(len(self.device_types)-1,0)
+        # the unknown device type should always be the last
+        self.device_types.insert(i, device_type)
 
     def get_device_type(self, device_type_name):
         for dt in self.device_types:
@@ -29,13 +33,13 @@ class Gateway:
 
     def __del__(self):
         if not(self.serial_port is None):
-            logging.debug("Closing COM port: " + self.serial_port)
+            logging.debug('Closing COM port: ' + self.serial_port)
             self.serial_port.close()
 
     def step(self, message):
         data = self.serial_port.readline()
         if len(data) > 0:
-            message += data.decode("utf-8")
+            message += data.decode('utf-8')
             eol = message[-1]
             if eol == '\n':
                 # complete message: share with devices
@@ -45,7 +49,7 @@ class Gateway:
         return message
 
     def run(self, com_port: str):
-        logging.info("Opening COM port " + com_port)
+        logging.info('Opening COM port ' + com_port)
         self.serial_port = serial.Serial(com_port, 57600, timeout=0.1)
 
         message = ''
@@ -53,7 +57,7 @@ class Gateway:
             self.step(message)
 
         self.serial_port.close()
-        logging.debug("Serial comms thread done")
+        logging.debug('Gateway run stopped')
 
     def send(self, pulses):
         p_str = 't:' + RFLinkTools.pulses_to_string(pulses) + '\n'
@@ -61,7 +65,8 @@ class Gateway:
         self.serial_port.write(p_str.encode('utf-8'))
 
     def __handle_message(self, message):
-        if message[0] == "r":
+        # received messages start with 'r'
+        if message[0] == 'r':
             now = datetime.now()
             try:
                 m = re.split('[:,]', message)[2:]
@@ -72,4 +77,13 @@ class Gateway:
                         # Note that the unknown device should always be at the end of this list
                         break
             except Exception as e:
-                logging.debug("Invalid message")
+                logging.debug('Invalid message')
+
+    def load(self, filename):
+        self.filename = filename
+        with open(self.filename, 'rb') as f:
+            self.device_types = pickle.load(file=f)
+
+    def save(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self.device_types, file=f)
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import logging
 from flask import Flask, jsonify
 import threading
 import argparse
+import time
+
 
 logging.getLogger().setLevel(logging.DEBUG)
 gateway = Gateway()
@@ -28,7 +30,7 @@ def command(device_type, device_id, command):
             raise Exception('no such device instance')
         pulses = di.execute_command(command)
         gateway.send(pulses)
-        return jsonify(result=True)
+        return jsonify(result=True, error='')
     except Exception as e:
         logging.info('command execution failed' + str(e))
         return jsonify(result=False, error=str(e))
@@ -36,18 +38,37 @@ def command(device_type, device_id, command):
 
 @app.route('/devices', methods=['GET'])
 def get_devices():
-    devices = {}
-    for device_type in gateway.device_types:
-        devices[device_type.type_name] = []
-        for device_instance in device_type.instances:
-            devices[device_type.type_name].append(device_instance.id)
-    return jsonify(devices=devices)
-
+    try:
+        devices = {}
+        for device_type in gateway.device_types:
+            devices[device_type.type_name] = []
+            for device_instance in device_type.instances:
+                devices[device_type.type_name].append(device_instance.id)
+        return jsonify(result=True, error='', devices=devices)
+    except Exception as e:
+        logging.info('could not get devices' + str(e))
+        return jsonify(result=False, error=str(e))
 
 @app.route('/device_types', methods=['GET'])
 def get_device_types():
-    types = [t.type_name for t in gateway.device_types]
-    return jsonify(device_types=types)
+    try:
+        types = [t.type_name for t in gateway.device_types]
+        return jsonify(result=True, error='', device_types=types)
+    except Exception as e:
+        logging.info('Error getting device types' + str(e))
+        return jsonify(result=False, error=str(e))
+
+@app.route('/add_device_type/<device_module>/<device_type>', methods=['GET'])
+def add_device_types(device_module, device_type):
+    try:
+        device_type_module = __import__(device_module)
+        device_type_class = getattr(device_type_module, device_type)
+        device_type_instance = device_type_class()
+        gateway.add_device_type(device_type_instance)
+        return jsonify(result=True, error='')
+    except Exception as e:
+        logging.info('Adding device type failed' + str(e))
+        return jsonify(result=False, error=str(e))
 
 
 @app.route('/device_instances/<device_type>', methods=['GET'])
@@ -57,10 +78,10 @@ def get_device_instances(device_type):
         if dt is None:
             raise Exception('no such device type')
         instances = [i.id for i in dt.instances]
-        return jsonify(instances=instances)
+        return jsonify(result=True, error='', instances=instances)
     except Exception as e:
         logging.info('listing device instances failed' + str(e))
-        return jsonify(error=str(e))
+        return jsonify(result=False, error=str(e))
 
 
 @app.route('/device_state/<device_type>/<device_id>', methods=['GET'])
@@ -72,12 +93,13 @@ def get_device_state(device_type, device_id):
         di = dt.get_instance(device_id)
         if di is None:
             raise Exception('no such device instance')
-        return jsonify(result=True, state=di.get_state())
+        return jsonify(result=True, error='', state=di.get_state())
     except Exception as e:
         logging.info('getting device state failed' + str(e))
         return jsonify(result=False, error=str(e))
 
 
+# todo: move the list of commands to the device_type
 @app.route('/device_commands/<device_type>/<device_id>', methods=['GET'])
 def get_device_commands(device_type, device_id):
     try:
@@ -87,7 +109,7 @@ def get_device_commands(device_type, device_id):
         di = dt.get_instance(device_id)
         if di is None:
             raise Exception('no such device instance')
-        return jsonify(result=True, state=di.get_commands())
+        return jsonify(result=True, error='', state=di.get_commands())
     except Exception as e:
         logging.info('getting device commands failed' + str(e))
         return jsonify(result=False, error=str(e))
@@ -101,23 +123,26 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
     parser = argparse.ArgumentParser(description='RFLink-alt-Gateway')
-    parser.add_argument('serialport', help='serial port')
+    parser.add_argument('serial', help='serial port')
     parser.add_argument('host', help='interface to use')
     parser.add_argument('port', type=int, help='port to listen to')
+    parser.add_argument('database', help='database file')
     args = parser.parse_args()
 
+    gateway.load(args.database)
     # Setup two somfy screens
-    somfy = SomfyRemoteType()
-    remote1 = SomfyRemoteInstance(code=0x09B8, remote=0x0F0101)
-    remote2 = SomfyRemoteInstance(code=0x099C, remote=0x0F0102)
-    somfy.add_instance(remote1)
-    somfy.add_instance(remote2)
-    gateway.add_device_type(somfy)
-
-    # Flamingo smoke alarm
-    gateway.add_device_type(RA20RFType())
-    gateway.add_device_type(UnknownDeviceType())
-
+    # somfy = SomfyRemoteType()
+    # somfy.add_instance(SomfyRemoteInstance(code=0x09B8, remote=0x0F0101))
+    # somfy.add_instance(SomfyRemoteInstance(code=0x099C, remote=0x0F0102))
+    # gateway.add_device_type(somfy)
+    #
+    # # Flamingo smoke alarm
+    # gateway.add_device_type(RA20RFType())
+    #
+    # gateway.add_device_type(UnknownDeviceType())
+    # gateway.save(args.database)
     threading.Thread(target=thread_runner, args=(args.host, args.port,)).start()
-    logging.debug('Starting Gateway at {serialport}'.format(serialport=args.serialport))
-    gateway.run(com_port=args.serialport)
+    logging.debug('Starting Gateway at {serial}'.format(serial=args.serial))
+ #   gateway.run(com_port=args.serialport)
+    while True:
+        time.sleep(0.1)
